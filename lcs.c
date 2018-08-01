de26b942e21adc2d5a8b959813d8038774006765
Renan Tomazini <renantomazini@gmail.com>
2018-08-01 19:54:33 -0300

    Update lcs.c

--- lcs.c
+++ lcs.c
@@ -1,15 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <locale.h>
+//#include <locale.h>
 
 void abre_arquivos(){
       FILE* f; FILE* f2;
+      int tam1,tam2;
+      char texto1[15],texto2[15];
 
     f = fopen("palavras1.txt", "r");
     f2 = fopen("palavras2.txt", "r");
 
+    while( (fscanf(f,"%s", texto1))!=EOF ){
+			printf("%s \n", texto1);}
+
+    while( (fscanf(f2,"%s", texto2))!=EOF ){
+            printf("%s \n", texto2);}
+
+    tam1 = strlen(texto1);
+    printf("%d",tam1);
+    tam2 = strlen(texto2);
+    printf("%d",tam2);
+
 }
+
+void preencher(int tam1, int tam2){
+    char texto1[tam1]; char texto2[tam2];
+    //fscanf(f,"%s", texto1);
+    //fscanf(f2,"%s", texto2);
+}
+
 void compara_arquivo(){
 
 }
@@ -17,7 +37,7 @@ void compara_arquivo(){
 
 
 int main(){
-setlocale  (LC_ALL, "Portuguese");
+//setlocale  (LC_ALL, "Portuguese");
 
 
     abre_arquivos();
